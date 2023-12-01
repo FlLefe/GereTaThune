abf93f2c846f048f4fc133c2b2e59d52b093e956
Lefebvre <lefebvre.florian9@gmail.com>
2023-12-01 19:35:02 +0100

    push avant de tout casser

--- app/controllers/financeController.js
+++ app/controllers/financeController.js
@@ -1,16 +1,28 @@
-const { Movement, Operation } = require('../models')
+const { Movement, Operation, Category } = require('../models')
 
 const financeController = {
     
     index: async (req, res) => {
-        console.log('req.session.user >>' + JSON.stringify(req.session.user, null, 2));
         
         const idUser = req.session.user.id
-        const movements = await Movement.findAll({
-            where: {user_id : idUser},
-            include : ['operation', 'category']
-        });
+        const movements = await Operation.findAll({
+            include : [{model : Movement, as: "movements",
+            include: 'category',
+            where : {user_id : idUser}}],
+            order: [
+                ['movements', 'category_id', 'ASC'],
+                ['movements', 'amount', 'DESC']
+              ]
+            
+        })
 
+        const movementByCategory = await Category.findAll({
+            include : 'movements',
+        })
+
+        console.log(JSON.stringify(movementByCategory, null, 2))
+
+        // console.log(JSON.stringify(movements, null, 2))
         const categories = await Operation.findAll({
             include: 'categories'
         })
@@ -19,9 +31,9 @@ const financeController = {
     
     addMovement: async (req, res) => {
         const { id } = req.params
-        const { type, amount, category_id } = req.body
+        const { type, amount, comment, category_id } = req.body
 
-        await Movement.create({type, amount, category_id, operation_id:id, user_id : req.session.user.id });
+        await Movement.create({type, amount, comment, category_id, operation_id:id, user_id : req.session.user.id });
 
         res.redirect('/finance');
     }
