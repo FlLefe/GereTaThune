b127974e5542bcd410e005d571317ad70305cabe
Lefebvre <lefebvre.florian9@gmail.com>
2023-12-05 13:30:56 +0100

    Installation Tailwind CLI et script

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/*.{html,js,css}",
+  "./app/views/*.ejs",
+  "./app/views/partials/*.ejs"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
